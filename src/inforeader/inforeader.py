6102e1647a7d0a8afa956d5da7337d890eeeee41
jisern <jisern@correo.ugr.es>
2021-07-20 09:27:05 +0200

    Minors

--- src/inforeader/inforeader.py
+++ src/inforeader/inforeader.py
@@ -6,6 +6,28 @@ import shutil
 
 
 class InfoReader(object):
+
+    FAIL_MSG = {
+        "id": "??",
+        "ts": "??",
+        "sync_min": "0",
+        "sync_max": "0",
+        "sync_mean": "0",
+        "link_delay_0": "0",
+        "link_delay_1": "0",
+        "link_delay_2": "0",
+        "link_delay_3": "0",
+        "port_role_0": "0",
+        "port_role_1": "0",
+        "port_role_2": "0",
+        "port_role_3": "0",
+        "node_status": "0",
+        "GM": "",
+        "path_trace1": "",
+        "path_trace2": "",
+        "last_failover": "0"
+    }
+
     def __init__(self):
         pass
 
@@ -48,43 +70,40 @@ class NodeReader(InfoReader):
         self.log_path = log_path
         self.simulate = simulate
         self.board = board
+        self.read_failures_count = 0
+        self.last_failover = 0
         super(NodeReader, self).__init__()
 
     def data(self):
 
-        try:
-            folders = os.listdir(self.log_path)
-        except KeyError:
-            raise Exception("Must specify a valid directory where the logs are")
-
-        date = [datetime.strptime(folder.replace('_', ' '), "%d%m%y %H%M") for folder in folders]
-
         if self.simulate:
+
+            try:
+                folders = os.listdir(self.log_path)
+                print(folders)
+            except KeyError:
+                raise Exception("Must specify a valid directory where the logs are")
+
+            date = [datetime.strptime(folder.replace('_', ' '), "%d%m%y %H%M") for folder in folders]
+
             newest = folders[date.index(min(date))]
-        else:
-            newest = folders[date.index(max(date))]
 
-        try:
-            logs = [each for each in os.listdir(os.path.join(self.log_path, newest, self.board)) if each.endswith('.log')]
-        except Exception as e:
-            print("Must specify a valid directory where the logs are")
-            return None
+            try:
+                logs = [each for each in os.listdir(os.path.join(self.log_path, newest, self.board)) if each.endswith('.log')]
+            except Exception as e:
+                print("Must specify a valid directory where the logs are")
+                return None
 
-        if self.simulate:
             logs.sort()
-        else:
-            logs.sort(reverse=True)
 
-        try:
-            log = logs[0]
-        except Exception as e:
-            return None
+            try:
+                log = logs[0]
+            except Exception as e:
+                return None
 
-        log_path = os.path.join(self.log_path, newest, self.board, log)
-        data = '{' + self._readChunk(log_path) + '}'
-        os.remove(log_path)
-
-        if self.simulate is True:
+            log_path = os.path.join(self.log_path, newest, self.board, log)
+            data = '{' + self._readChunk(log_path) + '}'
+            os.remove(log_path)
 
             count = 0
 
@@ -92,25 +111,43 @@ class NodeReader(InfoReader):
                 for file in files:
                     if file.endswith(".log"):
                         count += 1
-
             if count == 0:
 
                 if os.path.exists(os.path.join(self.log_path, newest, self.board)):
                     shutil.rmtree(os.path.join(self.log_path, newest, self.board))
-
                     if len(os.listdir(os.path.join(self.log_path, newest))) == 0:
                         print('{} vacía, borrado'.format(newest))
                         shutil.rmtree(os.path.join(self.log_path, newest))
                         folders.remove(newest)
                         date.remove(min(date))
-
                 return None
 
-        try:
-            data = json.loads(data)
-            data = data['data'][0]
-        except Exception as e:
-            data = None
-            print(e)
+            try:
+                data = json.loads(data)
+                data = data['data'][0]
+            except Exception as e:
+                data = self.FAIL_MSG
+                print(e)
+                return data
 
+        else:
+            data = self._readChunk(self.log_path)
+
+            try:
+                data = json.loads(data)['data'][0]
+            except Exception as e:
+                data = self.FAIL_MSG
+                print(e)
+                return data
+
+        last_fail_real = data['last_failover']
+
+        if data['last_failover'] == self.last_failover:
+            self.read_failures_count += 1
+            if self.read_failures_count >= 2:
+                data = self.FAIL_MSG
+        else:
+            self.read_failures_count = 0
+
+        self.last_failover = last_fail_real
         return data
--- src/systemconfigurer/applicationconfigurer.py
+++ src/systemconfigurer/applicationconfigurer.py
@@ -2,6 +2,7 @@ import json
 from requester import FivisRequester
 from handler import RequestHandler
 from requestformatter import *
+from inforeader import *
 import os
 from datetime import datetime
 from distutils.dir_util import copy_tree
@@ -21,32 +22,40 @@ class ApplicationConfigurer(object):
     def getNodeHandlers(self):
 
         try:
-            folders = os.listdir(self.log_folder)
-        except KeyError:
-            raise Exception("Must specify a valid directory where the logs are")
-
-        date = [datetime.strptime(folder.replace('_', ' '), "%d%m%y %H%M") for folder in folders]
-
-        if self.simulate:
-            newest = folders[date.index(min(date))]
-            copy_tree(self.log_folder, '../res/logs_sim')
-            self.log_folder = '../res/logs_sim'
-        else:
-            newest = folders[date.index(max(date))]
-
-        try:
-            boards = os.listdir(os.path.join(self.log_folder, newest))
+            files_folders = os.listdir(self.log_folder)
         except KeyError:
             raise Exception("Must specify a valid directory where the logs are")
 
         nodes_handlers = []
 
-        for board in boards:
-            node_reader = NodeReader(self.log_folder, board, self.simulate)
-            node_formatter = NodeFormatter(node_reader)
-            signal_set = '7sols_' + board
-            fivis_requester = self._getFivisRequester(signal_set, node_formatter.getSchema())
-            nodes_handlers.append(RequestHandler(node_formatter, fivis_requester))
+        if self.simulate:
+            date = [datetime.strptime(folder.replace('_', ' '), "%d%m%y %H%M") for folder in folders]
+            newest = files_folders[date.index(min(date))]
+            copy_tree(self.log_folder, '../res/logs_sim')
+            self.log_folder = '../res/logs_sim'
+
+            try:
+                boards = os.listdir(os.path.join(self.log_folder, newest))
+            except KeyError:
+                raise Exception("Must specify a valid directory where the logs are")
+
+            for board in boards:
+                node_reader = NodeReader(self.log_folder, board, self.simulate)
+                node_formatter = NodeFormatter(node_reader)
+                signal_set = '7sols_' + board
+                fivis_requester = self._getFivisRequester(signal_set, node_formatter.getSchema())
+                nodes_handlers.append(RequestHandler(node_formatter, fivis_requester))
+        else:
+
+            logs = files_folders
+
+            for log in logs:
+                board = log.replace('.log','').split('_')[1]
+                node_reader = NodeReader(self.log_folder+log, board, self.simulate)
+                node_formatter = NodeFormatter(node_reader)
+                signal_set = '7sols_' + board
+                fivis_requester = self._getFivisRequester(signal_set, node_formatter.getSchema())
+                nodes_handlers.append(RequestHandler(node_formatter, fivis_requester))
 
         return nodes_handlers
 
